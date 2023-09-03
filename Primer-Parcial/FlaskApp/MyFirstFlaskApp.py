51424d8f45be7ced2d099e28deb6790dd961a363
jvnko <a01746643@itesm.mx>
2023-09-03 13:25:10 +0000

    Project finished

---
+++ Primer-Parcial/FlaskApp/MyFirstFlaskApp.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, render_template
+app = Flask(__name__)
+
+@app.route('/', methods = ['GET'])
+def login():
+    return render_template('login.html')
+
+@app.route('/login', methods = ['POST','GET'])
+def loginCheck():
+    username = request.form['username']
+    password = request.form['password']
+
+    if username == username.capitalize() and password.isalnum():
+        return render_template('success.html')
+    else:
+        return render_template('login.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True, port=8081)
+    
--- Primer-Parcial/FlaskApp/app.py
+++ Primer-Parcial/FlaskApp/app.py
@@ -34,5 +34,6 @@ def json():
     return jsonify(data)
 
 
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, port=8081)
