85223e0adcbba2fe0b464af6c32b9643cc8b45bf
jvnko <a01746643@itesm.mx>
2023-09-03 06:55:58 +0000

    project finished

--- Primer-Parcial/Scripting-Application/grocery_start/main.py
+++ Primer-Parcial/Scripting-Application/grocery_start/main.py
@@ -1,19 +1,5 @@
-
 import csv
 
-# with open("sample_grocery.csv",'r') as file:
-#   csvreader = csv.reader(file)
-#   for row in csvreader:
-#     print(row)   
-
-# print('===================================')
-# with open("grocery_batch_1.csv",'r') as file:
-#   csvreader = csv.reader(file)
-#   for row in csvreader:
-#     print(row)   
-
-
-# data = [{'name': 'John Doe', 'age': 30}, {'name': 'Jane Doe', 'age': 25}]
 def write_list_of_dicts_to_csv(filename, data):
     with open(filename, 'w') as f:
         writer = csv.DictWriter(f, fieldnames=data[0].keys())
@@ -25,19 +11,23 @@ def read_csv_to_dict(filename):
     with open(filename, 'r') as f:
         reader = csv.DictReader(f)
         return list(reader)
-
-
-# def main(filename):
-#     data = read_csv_to_dict(filename)
-#     for row in data:
-#         print(row)
-#     return data
-
-# Press the green button in the gutter to run the script.
+    
 if __name__ == '__main__':
     dataSource = read_csv_to_dict('sample_grocery.csv')
     batchData = read_csv_to_dict('grocery_batch_1.csv')
-    # print(f'data source : {dataSource[0]}')
-    # print(f'batch : {batchData[0]}')
-    print(len(dataSource[0]))
+
+    for dictB in batchData:
+        for dictS in dataSource:
+            if dictB['SKU'] == dictS['SKU']:
+                print(f'same product found! ')
+                print('SKU: ', dictS['SKU'], 'Actual quantity =', dictS['Quantity'])
+                dictS['Quantity'] = int(dictS['Quantity']) + int(dictB['Quantity'])
+                print('SKU: ', dictS['SKU'], 'updated quantity =', dictS['Quantity'])
+                print('-'* 10)
+                print('Updating Source list....')
+    if dictB['SKU'] not in dataSource:
+        print('The following SKU was not found: ', dictB['SKU'], ' --It will be added to source')
+        dataSource.append(dictB)
+
+    write_list_of_dicts_to_csv('grocery_db.csv',dataSource)
 
